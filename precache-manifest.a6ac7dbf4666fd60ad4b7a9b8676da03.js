1f52d8c8bbbcfe0d70075865c753e6367a57fd0c
mr-anon <mr-anon@pop-os.localdomain>
2021-06-26 16:36:30 +0530

    Updates

---
+++ precache-manifest.a6ac7dbf4666fd60ad4b7a9b8676da03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02026b81aa2a49ed875615c81986e52c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84cf28114804c0959739",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "88448ffcfce5fdaf8e09",
+    "url": "/static/js/2.cfafeff4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cf28114804c0959739",
+    "url": "/static/js/main.b63baee4.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "e14b90eefbbbe8f89b20134a7713b3a4",
+    "url": "/static/media/mr-anon.e14b90ee.jpeg"
+  },
+  {
+    "revision": "1c7dfe70ac7aaceec90ab71671643ab2",
+    "url": "/static/media/resume.1c7dfe70.pdf"
+  }
+]);
